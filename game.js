c0b75cca6c0abefc46f63e73616c39df7b5c65dc
Architap18 <architapal1806@gmail.com>
2026-02-23 17:02:21 +0530

    new feature added

--- game.js
+++ game.js
@@ -1,28 +1,66 @@
 let userScore = 0;
 let botScore = 0;
-
+let totalMoves = 0;
+const maxMoves = 10;
 const choices = ["rock", "paper", "scissors"];
-
 function play(userChoice) {
+  if (totalMoves >= maxMoves) {
+    return; //stop
+  }
   const botChoice = choices[Math.floor(Math.random() * 3)];
   const result = document.getElementById("result");
 
   if (userChoice === botChoice) {
     result.innerText = `Draw! Bot chose ${botChoice}`;
-  } else if (
+  } 
+  else if (
     (userChoice === "rock" && botChoice === "scissors") ||
     (userChoice === "paper" && botChoice === "rock") ||
     (userChoice === "scissors" && botChoice === "paper")
   ) {
     userScore++;
     result.innerText = `You Win! Bot chose ${botChoice}`;
-  } else {
+  } 
+  else {
     botScore++;
     result.innerText = `You Lose! Bot chose ${botChoice}`;
   }
 
+  totalMoves++;
+
   document.getElementById("userScore").innerText = userScore;
   document.getElementById("botScore").innerText = botScore;
+
+  checkWinner();
+}
+
+function checkWinner() {
+  if (totalMoves === maxMoves) {
+
+    const result = document.getElementById("result");
+
+    if (userScore > botScore) {
+      result.innerText = "🎉 You Won The Game!";
+    } 
+    else if (botScore > userScore) {
+      result.innerText = "💻 Bot Won The Game!";
+    } 
+    else {
+      result.innerText = "🤝 It's a Draw!";
+    }
+
+    setTimeout(resetGame, 2000);
+  }
+}
+
+function resetGame() {
+  userScore = 0;
+  botScore = 0;
+  totalMoves = 0;
+
+  document.getElementById("userScore").innerText = 0;
+  document.getElementById("botScore").innerText = 0;
+  document.getElementById("result").innerText = "Game Reset!";
 }
 
 function logout() {
--- sign.js
+++ sign.js
@@ -1,26 +1,20 @@
-// In-memory users (reset on refresh)
 let users = [];
-
 function signup() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   const msg = document.getElementById("msg");
-
   if (!username || !password) {
     msg.innerText = "Please fill all fields";
     return;
   }
-
   const exists = users.find(u => u.username === username);
   if (exists) {
     msg.innerText = "User already exists";
     return;
   }
-
   users.push({ username, password });
   msg.innerText = "Signup successful! Now login.";
 }
-
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
